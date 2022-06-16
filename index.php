2ee32c584b57064f35e2bf57a5938842db04b15b
Cooper Bedin <cooper.bedin@berkeley.edu>
2022-06-15 17:13:17 -0700

    style updates and testing php

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Cooper Bedin</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="script.js"></script>
+  </head>
+  <body>
+    <header>
+      <div id="pfp">
+        <img src="imgs/pfp.jpg" alt="PFP">
+      </div>
+      <table>
+        <tr>
+          <th colspan="2">Cooper Bedin</th>
+        </tr>
+        <tr>
+          <td>[kupɚ bɛdin]</td>
+          <td>(any/all)</td>
+        </tr>
+        <tr>
+          <td colspan="2">cbedin (at) ucsb.edu</td>
+        </tr>
+        <tr>
+          <td colspan="2"><a href="https://twitter.com/ceaslugs">@ceaslugs</a></td>
+        </tr>
+        <tr>
+          <td colspan="2"><a href="https://github.com/cbedin">cbedin</a></td>
+        </tr>
+      </table>
+    </header>
+    <div id="main">
+      <p>Hello!!!!</p>
+    </div>
+  </body>
+</html>
