a9786adf356331b974471f34aa7dc0b1ce7bba78
Cooper Bedin <cooper.bedin@berkeley.edu>
2022-06-17 00:00:43 -0700

    fixing formatting and removing footer

--- src/script.js
+++ src/script.js
@@ -1,5 +1,4 @@
 $(function() {
     $("head").load("https://cbedin.github.io/src/head.html");
     $("header").load("https://cbedin.github.io/src/header.html");
-    $("footer").load("https://cbedin.github.io/src/footer.html");
 });
